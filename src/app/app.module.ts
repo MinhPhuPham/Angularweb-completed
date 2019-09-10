f16a8f8c9e479b05759ddb8289c07184a92efbe3
phamminhphu <phamminhphueur@gmail.com>
2019-09-10 20:41:12 +0700

    Routing and config file

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'ng-starrating';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import {NgxPaginationModule} from 'ngx-pagination';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavMainComponent } from './nav-main/nav-main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShoppingHomeComponent } from './shopping-home/shopping-home.component';
+import { WrongpathComponent } from './wrongpath/wrongpath.component';
+import { Lab01Component } from './homework/lab01/lab01.component';
+import { Lab02Component } from './homework/lab02/lab02.component';
+import { DetailProductsComponent } from './homework/lab01/detail-products/detail-products.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DetailStudentComponent } from './homework/lab02/detail-student/detail-student.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMainComponent,
+    FooterComponent,
+    ShoppingHomeComponent,
+    WrongpathComponent,
+    Lab01Component,
+    Lab02Component,
+    DetailProductsComponent,
+    LoginComponent,
+    RegisterComponent,
+    DetailStudentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RatingModule,
+    Ng2SearchPipeModule,
+    FormsModule,
+    NgxPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
