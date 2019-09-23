20889182615b2da93eaa406317ccbac6bfd8eeec
phamminhphu <phamminhphueur@gmail.com>
2019-09-24 00:06:41 +0700

    Build UI for assignment

--- src/app/assignment/list-subjects/list-subjects.component.ts
+++ src/app/assignment/list-subjects/list-subjects.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import {subjects} from '../subjects'
 @Component({
   selector: 'app-list-subjects',
   templateUrl: './list-subjects.component.html',
   styleUrls: ['./list-subjects.component.css']
 })
 export class ListSubjectsComponent implements OnInit {
-
-  constructor() { }
-
+  pageOfItems: Array<any>;
+  config: any;
+  constructor() { 
+    this.config = {
+      itemsPerPage: 6,
+      currentPage: 1,
+      totalItems: this.listSubjects.length
+    };
+  }
+  searchText
+  listSubjects=[]
   ngOnInit() {
+    this.listSubjects=[...subjects]
+  }
+  pageChanged(event){
+    this.config.currentPage = event;
   }
 
+  onChangePage(pageOfItems: Array<any>) {
+    // update current page of items
+    this.pageOfItems = pageOfItems;
+}
 }
