8e04b20d9990cf03a1bbbf80067caf5d5d6c767c
phamminhphu <phamminhphueur@gmail.com>
2019-10-06 23:35:58 +0700

    Assignment fixed

---
+++ src/app/homework/lab05/lab05.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab05',
+  templateUrl: './lab05.component.html',
+  styleUrls: ['./lab05.component.css']
+})
+export class Lab05Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
